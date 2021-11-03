01bbe94ebefe00e362a8770951d3c861daba1624
Aldrich Reboja <aktreboja@gmail.com>
2021-11-03 09:32:05 -0700

    11/3: tftmacro initialization

---
+++ src/Pages/DamageCalculator/DamageCalculator.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Table } from '@mui/material'
+
+export default function DamageCalculator() {
+    return (
+        <div>
+            <h1>Damage Calculator</h1>
+            <Table />
+        </div>
+    )
+}
